8420a8fc82d832db371cbe8856fe674f20477301
Andreas V <avou@kth.se>
2021-10-07 14:14:19 +0200

    testar grupp 14s kod

--- robotmain.c
+++ robotmain.c
@@ -1,102 +1,113 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include "brick.h"
 #include <unistd.h>
-#define Sleep( msec ) usleep(( msec ) * 1000 ) /* Definerar sleep, Sleep(1000)= 1 sekund */
-#define MOTOR_RIGHT    OUTA
-#define MOTOR_LEFT    OUTB
-#define MOTOR_C    OUTC
-#define MOTOR_D    OUTD
-#define SENSOR_TOUCH IN1
-#define SENSOR_2 IN2
-#define SENSOR_3 IN3
-#define SENSOR_4 IN4
-#define MOTOR_BOTH     ( MOTOR_LEFT | MOTOR_RIGHT ) /* Bitvis ELLER ger att båda motorerna styrs samtidigt */
-int max_hastighet;         /* variabel för max hastighet på motorn */ 
-POOL_T touch_sensor;
-POOL_T sonic_sensor;
-void move_back();
-void move_straight();
-void move_right();
-void move_left();  
+#include <string.h>
+#include <ev3.h>
+#include <ev3_port.h>
+#include <ev3_sensor.h>
 
+#define Sleep( msec ) usleep(( msec ) * 1000 ) /* Definerar sleep, Sleep(1000)= 1 sekund */
+
+#define MOTOR_RIGHT        OUTA   /*Vänster motor*/
+#define MOTOR_LEFT        OUTB    /*Höger motor*/
+#define MOTOR_C            OUTC    /*Lägg servomotor här*/
+#define MOTOR_D            OUTD
+#define SENSOR_TOUCH    IN1    /*Avståndssensor*/
+#define SENSOR_US        IN2     
+#define SENSOR_3        IN3
+#define SENSOR_4        IN4
+#define MOTOR_BOTH         ( MOTOR_LEFT | MOTOR_RIGHT ) /* Bitvis ELLER ger att båda motorerna styrs samtidigt */
+
+int max_hastighet;         /* variabel för max hastighet på motorn */
+int max_hastighet_c;
+POOL_T touchSensor, us_sensor;  
+
+
+/*Här börjar main koden*/
 int main( void )
 {  
-  if ( !brick_init()) return ( 1 ); /* Initialiserar EV3-klossen */
-  printf( "*** ( EV3 ) Hello! ***\n" );
-  Sleep( 2000 );
-  
-  if ( tacho_is_plugged( MOTOR_BOTH, TACHO_TYPE__NONE_ )) {  /* TACHO_TYPE__NONE_ = Alla typer av motorer */        
-    max_hastighet = tacho_get_max_speed( MOTOR_LEFT, 0 );/* Kollar maxhastigheten som motorn kan ha */        
-    tacho_reset( MOTOR_BOTH );    
-  } 
-  else {        
-    printf( "Anslut vänster motor i port A,\n""Anslut höger motor i port B.\n");
-    brick_uninit();        
-    return (0);  /* Stänger av sig om motorer ej är inkopplade */    
-  } 
-  
-  touch_sensor = sensor_search( LEGO_EV3_TOUCH ); // Registrerar en touch sensorpå touchSensor-variabeln
-	sonic_sensor = sensor_search(LEGO_EV3_US);
-  
-  touch_set_mode_touch(touch_sensor); // anger vilken "mode" sensorn skall ha
-  /* 
-  move_straight();
-  Sleep( 1000 );
-        
-  tacho_stop( MOTOR_BOTH );
-  move_back();
-  Sleep( 1000 );
-  
-  tacho_stop( MOTOR_BOTH );
-  move_left();
+    /* uint8_t sn_touch, sn_color, sn_US; */
     
-  Sleep( 4000 );
+    if ( !brick_init()) return ( 1 ); /* Initialiserar EV3-klossen */
+    printf( "*** ( EV3 ) Hello! ***\n" );
+    Sleep( 2000 );
     
-  move_right();
+    if ( tacho_is_plugged( MOTOR_BOTH, TACHO_TYPE__NONE_ )) {  /* TACHO_TYPE__NONE_ = Alla typer av motorer */
+        max_hastighet = tacho_get_max_speed( MOTOR_LEFT, 0 );    /* Kollar maxhastigheten som motorn kan ha */
+        max_hastighet_c = tacho_get_max_speed(MOTOR_C,0);   
+
+        tacho_reset( MOTOR_BOTH );
+    } else {
+        printf( "Anslut vänster motor i port A,\n"
+        "Anslut höger motor i port B.\n"
+        );
+          brick_uninit();
+        return ( 0 );  /* Stänger av sig om motorer ej är inkopplade */
+    }
+    
+    
+    //tacho_set_speed_sp( MOTOR_BOTH, max_hastighet * 0.5 );  // Sätter hastigheten på båda motorerna till 50% av maxhastigheten
+    /* Om man vill köra bakåt anger man negativ hastighet, till exempel max_hastighet * (-0.5) */
+    
+    /*
+    tacho_run_forever(  MOTOR_BOTH );
+    tacho_stop( MOTOR_LEFT );
+    tacho_stop( MOTOR_RIGHT );
+    */
+    
+    printf("test1 \n");
+    us_sensor=sensor_search( LEGO_EV3_US );
+    us_set_mode_us_dist_cm(us_sensor);
+    int distance;
+    int tempdist;
+    distance=sensor_get_value(0, us_sensor, 0); //Se nuvarande avstånd
+    printf("test2 \n");
+    
+    tacho_set_speed_sp( MOTOR_LEFT, max_hastighet * -0.1 );
+    tacho_run_forever(  MOTOR_LEFT ); //Snurra runt
+    tacho_set_speed_sp( MOTOR_RIGHT, max_hastighet * 0.1 );
+    tacho_run_forever(  MOTOR_RIGHT ); //Snurra runt
+
+    while(1)  //while true
+    {
+    tempdist=sensor_get_value(0, us_sensor, 0);    //Tar avstånd
         
-  Sleep( 4000 );
-        
-  tacho_stop( MOTOR_RIGHT );
- 
- */ 
-  move_straight();
+        if (tempdist<distance)  //Om avståndet minskar
+        {
+            distance=tempdist;      //Spara nya värdet som distance
+            printf("%d \n", distance);
+            Sleep(50);
+        }    
+        else if (tempdist>distance && tempdist<530 && tempdist>470) //Om avståndet ökar och inom intervall då stanna
+        {
+            break;
+        }
+    }
+    tacho_stop(  MOTOR_BOTH ); //stanna motor
+    Sleep(50);
+    tacho_set_speed_sp( MOTOR_LEFT, max_hastighet * 0.1 );
+    tacho_run_forever(MOTOR_BOTH);
+    Sleep(15000);
+    tacho_stop(MOTOR_BOTH);
+    tacho_set_speed_sp( MOTOR_BOTH, max_hastighet * -0.1 );
+    tacho_run_forever(MOTOR_BOTH);
+    Sleep(3100);
+    tacho_stop(MOTOR_BOTH);
+    tacho_set_speed_sp( MOTOR_BOTH, max_hastighet * 0.1 );
+    tacho_run_forever(MOTOR_RIGHT);
+    Sleep(3000);
+    tacho_stop(MOTOR_RIGHT);
+    Sleep(100);
+    tacho_set_speed_sp( MOTOR_BOTH, max_hastighet * 0.3 );
+    tacho_run_forever(MOTOR_BOTH);
+    Sleep(20000);
+    tacho_set_speed_sp( MOTOR_C, max_hastighet_c * -0.3 );
+    tacho_run_forever(MOTOR_C);
+    Sleep(1000);
+    tacho_stop(MOTOR_C);
 
-  printf( "dyinfsdsg...\n" );    
-  us_set_mode_us_dist_cm(sonic_sensor);
-	int us_distance; /* kan heta vad som helst */
-  us_distance = (sensor_get_value(0, sonic_sensor, 0));
-  printf("%d",us_distance);
-  while(us_distance < 10){
-  printf("%d",us_distance);
+    brick_uninit();
+    printf( "dying...\n" );
+    return ( 0 );
 
-    Sleep( 4000 );
-  }//Så länge touch-sensorn inte ärintryckt kommer while-loopen köras
-
-
-  /*while(!sensor_get_value(0, touch_sensor, 0)); //Så länge touch-sensorn inte ärintryckt kommer while-loopen köras
-  move_back();
-    */    
-  Sleep( 4000 );
-  tacho_stop( MOTOR_BOTH );
-   brick_uninit();
-  printf( "dying...\n" );    
-  return (0);
 }
-
-void move_left(){
-  tacho_set_speed_sp( MOTOR_BOTH, max_hastighet * 0.1 );
-  tacho_run_forever( MOTOR_LEFT );
-}
-
-void move_right(){
-  tacho_stop( MOTOR_LEFT );
-  tacho_run_forever( MOTOR_RIGHT );
-}
-
-void move_straight(){
-  tacho_set_speed_sp( MOTOR_BOTH, max_hastighet * 0.1 );// Sätter hastighetenpå båda motorerna till 50% av maxhastigheten/* Om man vill köra bakåt anger man negativ hastighet, till exempel max_hastighet * (-0.5) */
-  tacho_run_forever( MOTOR_BOTH ); 
-}
-
-
-//rgtjiraegoiarje
